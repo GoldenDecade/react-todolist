30c55d75406af88c7eb4da897b5fbd4f1db39dd4
wangqiang8 <wangqiang8@kingsoft.com>
2020-08-28 07:25:23 +0800

    shouldComponentUpdate的使用

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -10,7 +10,15 @@ class TodoItem extends Component {
         const {delTask, index} = this.props;
         delTask(index);
     }
+    // 由于父组件的state更新了，父组件的render执行，导致子组件也要更新，但是子组件的props根本没有更新，此时更新子组件浪费性能
+    // 所以要通过 shouldComponentUpdate 判断子组件是否需要更新，从而提升性能
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        console.log('shouldComponentUpdate');
+        return nextProps.task !== this.props.task;
+    }
+
     render(){
+        console.log('render');
         const {task} = this.props;
         return (
             <div onClick={this.handleDelTask}>{task}</div>
