7723e8c4b84bdcbec83b6c6beb391d8ca29467c5
wangqiang8 <wangqiang8@kingsoft.com>
2020-08-29 19:19:25 +0800

    TransitionGroup 使用

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
 import React, {Component, Fragment} from 'react';
 import './App.css';
-import { CSSTransition } from 'react-transition-group';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            show: true
+            list: []
         };
         this.handleClick = this.handleClick.bind(this);
     }
     handleClick() {
+        let item = Math.random();
         this.setState((prevState) => ({
-            show: !prevState.show
-        }))
+            list: [...prevState.list, item]
+        }));
     }
     render(){
         return (
             <Fragment>
-                <CSSTransition
-                    in={this.state.show}
-                    timeout={1000}
-                    classNames='fade'
-                    unmountOnExit
-                    onEntered={(el)=> console.log(el)}
-                    appear={true}
-                >
-                    <div >hello</div>
-                </CSSTransition>
+                <TransitionGroup>
+                    {
+                        this.state.list.map((item, index) => {
+                            return (
+                                <CSSTransition
+                                    timeout={1000}
+                                    classNames='fade'
+                                    unmountOnExit
+                                    onEntered={(el)=> console.log(el)}
+                                    appear={true}
+                                    key={item}
+                                >
+                                    <div >{item}</div>
+                                </CSSTransition>
+                            )
+                        })
+                    }
+                </TransitionGroup>
                 <button onClick={this.handleClick}>toggle</button>
             </Fragment>
         )
