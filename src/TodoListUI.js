d2077ddd7f4251e4f149159508394c5e1b5399d6
wangqiang8 <wangqiang8@kingsoft.com>
2020-08-30 19:55:13 +0800

    无状态组件

--- src/TodoListUI.js
+++ src/TodoListUI.js
@@ -1,42 +1,40 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { Button, Input, List } from 'antd';
 // 由于 antd 组件的默认文案是英文，所以需要修改为中文
 import 'antd/dist/antd.css';
 
-class TodoListUI extends Component {
-    render(){
-        return (
-            <>
-                { /*这样写注释*/ }
-                <div>
-                    {/*这里这样写注释*/}
-                    <label htmlFor="task">
-                        任务：
-                        <Input
-                            id="task"
-                            style={{"width": "200px", "marginRight": "10px"}}
-                            type="text"
-                            value={this.props.inputValue}
-                            onChange={this.props.handleInputChange}
-                        />
-                    </label>
-                    <Button
-                        type="primary"
-                        onClick={this.props.handleClick}>提交</Button>
-                </div>
-                <List
-                    header={<div>Today Task</div>}
-                    bordered
-                    dataSource={this.props.list}
-                    renderItem={(item, index) => (
-                            <List.Item
-                                onClick={() => {this.props.delTask(index)}}
-                            >{item}</List.Item>
-                        )
-                    }
-                />
-            </>
-        )
-    }
-}
-export default TodoListUI;
+// 无状态组件
+export default (props) => {
+    return (
+        <>
+            { /*这样写注释*/ }
+            <div>
+                {/*这里这样写注释*/}
+                <label htmlFor="task">
+                    任务：
+                    <Input
+                        id="task"
+                        style={{"width": "200px", "marginRight": "10px"}}
+                        type="text"
+                        value={props.inputValue}
+                        onChange={props.handleInputChange}
+                    />
+                </label>
+                <Button
+                    type="primary"
+                    onClick={props.handleClick}>提交</Button>
+            </div>
+            <List
+                header={<div>Today Task</div>}
+                bordered
+                dataSource={props.list}
+                renderItem={(item, index) => (
+                    <List.Item
+                        onClick={() => {props.delTask(index)}}
+                    >{item}</List.Item>
+                )
+                }
+            />
+        </>
+    );
+};
